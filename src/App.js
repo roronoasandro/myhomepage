a539aba8ec1a738b62a8e33ffe014cbc655cf3cf
Sandro SIhaloho <sysihaloho@gmail.com>
2023-02-26 00:16:40 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Roronoasandro
+          Learn React with Roronoasandro dong!
         </a>
       </header>
     </div>
