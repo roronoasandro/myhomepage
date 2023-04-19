441742cbf21c915f751d584a52bf87702272ef7b
roronoasandro <roronoasandro@gmail.com>
2023-04-19 18:05:38 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Roronoasandro dong!
+          Learn React with Roronoasandro aja dong!
         </a>
       </header>
     </div>
